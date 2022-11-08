03a9386bacbcaf80dc2023bdd0722e753be3d3a9
Joao Gomes <joaoguilherme102030@gmail.com>
2022-11-07 22:19:16 -0300

    Create a Home screen and separate Agents List Screen

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:valorant_flutter/app/views/agent_detail/agent_detail_screen.dart';
-import 'package:valorant_flutter/app/views/home/home_screen.dart';
+import 'package:valorant_flutter/app/views/agents_list/agents_list.dart';
+import 'package:valorant_flutter/app/views/home_screen/home_screen.dart';
 
 class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -13,9 +14,11 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
         '/home': (context) => const HomeScreen(),
+        '/agentsList': (context) => const AgentsList(),
         '/agentDetail': (context) => const AgentDetailScreen(),
       },
     );
