4a233bb0c1fb2d7394c72c7e6fb10ffcce5aa681
Joao Gomes <joaoguilherme102030@gmail.com>
2022-12-09 23:35:49 -0300

    change design of map list

--- lib/app/map/views/map_list_screen.dart
+++ lib/app/map/views/map_list_screen.dart
@@ -4,7 +4,7 @@ import 'package:get_it/get_it.dart';
 import 'package:scroll_snap_list/scroll_snap_list.dart';
 import 'package:valorant_flutter/app/map/controller/map_list_controller.dart';
 
-import 'components/map_card_widget.dart';
+import 'components/map_tile_widget.dart';
 
 class MapListScreen extends StatefulWidget {
   const MapListScreen({Key? key}) : super(key: key);
@@ -26,37 +26,29 @@ class _MapListScreenState extends State<MapListScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Flexible(
-          flex: 2,
-          child: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(top: 32, bottom: 32),
-              child: Image(
-                height: 56,
-                image: AssetImage(
-                  'assets/valorant_icon.png',
-                ),
+        const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 32),
+            child: Image(
+              height: 56,
+              image: AssetImage(
+                'assets/valorant_icon.png',
               ),
             ),
           ),
         ),
         Flexible(
-          flex: 6,
+          flex: 9,
           child: Observer(builder: (context) {
-            return SizedBox(
-              height: MediaQuery.of(context).size.height * 0.7,
-              child: ScrollSnapList(
-                  selectedItemAnchor: SelectedItemAnchor.START,
-                  itemBuilder: ((context, index) {
-                    var actualMap = controller.mapsList[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 24.0),
-                      child: MapCardWidget(map: actualMap),
-                    );
-                  }),
-                  itemCount: controller.mapsList.length,
-                  itemSize: MediaQuery.of(context).size.width * 0.5,
-                  onItemFocus: (index) {}),
+            return ListView.builder(
+              itemBuilder: ((context, index) {
+                var actualMap = controller.mapsList[index];
+                return Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: MapTileWidget(map: actualMap),
+                );
+              }),
+              itemCount: controller.mapsList.length,
             );
           }),
         ),
