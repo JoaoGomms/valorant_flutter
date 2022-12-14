0c5999ca15cedc4610a09e9b66602ff920bc2f4b
Joao Gomes <joaoguilherme102030@gmail.com>
2022-12-13 23:44:00 -0300

    fix: change appbar height

--- lib/core/components/top_title_valorant.dart
+++ lib/core/components/top_title_valorant.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget topTitleValorant({required String title}) {
   return Padding(
-    padding: const EdgeInsets.only(top: 24, bottom: 48),
+    padding: const EdgeInsets.only(top: 24, bottom: 24),
     child: Text(
       title,
       style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
